dc6a02b06124b27c7fe403eba20534ff2a7ba7d9
AliMoosaei <ali.moosaei.big@gmail.com>
2023-06-09 00:13:40 +0330

    Complated register user Action in Register Controller

---
+++ ToDoProject/Web_API/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private ToDo.Application.Services.User_Service.IUserService _UserService;
+        public RegisterController(ToDo.Application.Services.User_Service.IUserService userservice)
+        {
+                _UserService= userservice;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] ToDo.Application.DTOs.RegisterUser_DTO Register_Data)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("ارسال اطلاعات به درستی انجام نشده است");
+            }
+
+           var Resualt_Add_User =  await _UserService.RegisterUser(Register_Data);
+
+            if(Resualt_Add_User)
+            {
+                return Ok("کاربر با موفقیت ثبت نام شد");
+            }
+            return BadRequest("در ثبت نام کاربر مشکلی رخ داد");
+        }
+    }
+}
